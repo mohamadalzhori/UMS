606a6724dda40f5aff8f9619b36f62af625a4697
Mohamad Alzhori <mohamadalzhori01@gmail.com>
2024-07-24 22:14:34 +0300

    Localization, NOT FINISHED

---
+++ UMS.API/Controllers/v1/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace UMS.API.Controllers.v1;
+
+[ApiController]
+[Route("Test")]
+[ApiVersion(1)]
+public class TestController(IStringLocalizer<TestController> _localizer) : ControllerBase
+{
+
+   [HttpGet("Test")]
+   public ActionResult<string> Test([FromQuery] string culture = "en-US")
+   {
+      // Validate and set the culture
+      if (string.IsNullOrWhiteSpace(culture) || !CultureInfo.GetCultures(CultureTypes.SpecificCultures).Any(c => c.Name == culture))
+      {
+         culture = "en-US"; // Default to "en-US" if invalid culture is provided
+      }
+
+      var cultureInfo = new CultureInfo(culture);
+      CultureInfo.CurrentCulture = cultureInfo;
+      CultureInfo.CurrentUICulture = cultureInfo;
+
+      // Get the localized message
+      var localizedMessage = _localizer["Hello"];
+
+      return Ok(localizedMessage);
+   }
+   
+   
+}
--- UMS.API/Program.cs
+++ UMS.API/Program.cs
@@ -1,12 +1,15 @@
+using System.Globalization;
 using Asp.Versioning;
 using Asp.Versioning.ApiExplorer;
 using Hangfire;
 using Hangfire.PostgreSql;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Localization;
 using UMS.Application;
 using UMS.Common.Converters;
 using Microsoft.AspNetCore.OData;
+using Microsoft.Extensions.Options;
 using Microsoft.OData.Edm;
 using Microsoft.OData.ModelBuilder;
 using Microsoft.OpenApi.Models;
@@ -20,21 +23,46 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Host.UseSerilog((context, configuration) => 
+builder.Services.AddLocalization();
+builder.Services.Configure<RequestLocalizationOptions>(
+    options =>
+    {
+        var supportedCultures = new List<CultureInfo>
+        {
+            new CultureInfo("en-US"),
+            new CultureInfo("en-AU"),
+            new CultureInfo("de-DE")
+        };
+
+        options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+        options.SupportedCultures = supportedCultures;
+        options.SupportedUICultures = supportedCultures;
+    });
+
+
+builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
 
 
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddPersistence(builder.Configuration);
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(new TimeOnlyJsonConverter());
-}).AddOData(options =>
-    options.Select().Filter().OrderBy().Expand().Count().AddRouteComponents("odata", GetEdmModel()));
+builder.Services.AddControllers()
+    .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new TimeOnlyJsonConverter()); })
+    .AddOData(options =>
+        options.Select().Filter().OrderBy().Expand().Count().AddRouteComponents("odata", GetEdmModel()));
+
+
+
+
+
+
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+
 builder.Services.AddProblemDetails();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -44,7 +72,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGen(options =>
 {
     // Add a Swagger document for each discovered API version
-    var provider =builder.Services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+    var provider = builder.Services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
     foreach (var description in provider.ApiVersionDescriptions)
     {
         options.SwaggerDoc(description.GroupName, new OpenApiInfo()
@@ -79,6 +107,8 @@ builder.Services.AddHangfire(config =>
 builder.Services.AddHangfireServer();
 
 var app = builder.Build();
+var localizeOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(localizeOptions.Value);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -91,10 +121,10 @@ if (app.Environment.IsDevelopment())
         var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
         foreach (var description in provider.ApiVersionDescriptions)
         {
-            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
         }
     });
-    
 }
 
 app.UseHttpsRedirection();
@@ -102,6 +132,7 @@ app.UseHttpsRedirection();
 app.UseMiddleware<RequestContextLogging>(); // push correlationId before logging
 
 app.UseSerilogRequestLogging();
+
 
 app.UseAuthorization();
 
@@ -111,7 +142,8 @@ app.MapControllers();
 
 app.UseHangfireDashboard("/jobs");
 
-RecurringJob.AddOrUpdate("Daily test message" ,() => EmailSender.Send("2d5edbec5d@emaildbox.pro", "test", "testis"), Cron.Daily(12,0));
+RecurringJob.AddOrUpdate("Daily test message", () => EmailSender.Send("2d5edbec5d@emaildbox.pro", "test", "test"),
+    Cron.Daily(12, 0));
 
 app.MapHealthChecks("healthz", new HealthCheckOptions
 {
